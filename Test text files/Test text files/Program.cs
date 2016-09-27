f423d7f0d67b888580be3e6be2a2b8b97f34c48c
Rochendiil <luukjeberg@live.nl>
2016-09-27 13:16:19 +0200

    Test Text to File Write

---
+++ Test text files/Test text files/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Test_text_files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            lines.Add("left");
+            lines.Add("right");
+            lines.Add("up");
+            File.WriteAllLines(@"C:\Users\Luuk\Dropbox\Maatwerk Proftaakgroep\Textfile\Lines.txt", lines);
+
+        }
+    }
+}
