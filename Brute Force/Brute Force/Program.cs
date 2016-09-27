77c2a36642507549ae170a4d7f0b2410962037e4
Rochendiil <luukjeberg@live.nl>
2016-09-27 14:44:21 +0200

    Eerste opzet Algoritme

---
+++ Brute Force/Brute Force/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Brute_Force
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int prevscore = 0;
+            Random r = new Random();
+            List<string> Toetsen = new List<string>();
+            Toetsen.Add("left");
+            Toetsen.Add("right");
+            Toetsen.Add("up");
+            List<string> combos = new List<string>();
+            List<string> combostemp = new List<string>();
+            while (0 == 0){
+                combostemp.Add(Toetsen[r.Next(0, 3)]);
+                File.WriteAllLines(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Lines.txt", combostemp);
+                if(score >= prevscore)
+                {
+                    combos = combostemp;
+                    prevscore = score;
+                }
+            }
+        }
+    }
+}
