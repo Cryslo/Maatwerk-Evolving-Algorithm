ea6412c68cf58ab44dd97db45432f418db5cc8a5
Rochendiil <luukjeberg@live.nl>
2016-09-29 10:29:04 +0200

    Aanpassingen
    2 extra toetsen

--- Brute Force/Brute Force/Program.cs
+++ Brute Force/Brute Force/Program.cs
@@ -9,20 +9,27 @@ namespace Brute_Force
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             int score = 0;
             int prevscore = 0;
+            int milliseconds = 200;
+            bool dead = false;
             Random r = new Random();
             List<string> Toetsen = new List<string>();
             Toetsen.Add("left");
             Toetsen.Add("right");
             Toetsen.Add("up");
+            Toetsen.Add("upright");
+            Toetsen.Add("upleft");
             List<string> combos = new List<string>();
             List<string> combostemp = new List<string>();
-            while (0 == 0){
-                combostemp.Add(Toetsen[r.Next(0, 3)]);
+            while (dead  == false)
+             {
+                combostemp.Add(Toetsen[r.Next(0, 5)]);
                 File.WriteAllLines(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Lines.txt", combostemp);
+                Task.Delay(milliseconds);
                 if(score >= prevscore)
                 {
                     combos = combostemp;
