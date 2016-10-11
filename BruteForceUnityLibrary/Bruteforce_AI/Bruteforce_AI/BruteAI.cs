c210076f683c2079f2c6d8719602cad3a0c312e9
Rochendiil <luukjeberg@live.nl>
2016-10-11 11:25:09 +0200

    Jump bool. Clear combo

--- BruteForceUnityLibrary/Bruteforce_AI/Bruteforce_AI/BruteAI.cs
+++ BruteForceUnityLibrary/Bruteforce_AI/Bruteforce_AI/BruteAI.cs
@@ -9,6 +9,7 @@ namespace Bruteforce_AI
         public List<Button> combos = new List<Button>();
         public List<Button> combostemp = new List<Button>();
         float prevscore = 0;
+        float prevtime;
         public List<Button> GenerateMovement()
         {
             combostemp.Clear();
@@ -17,7 +18,7 @@ namespace Bruteforce_AI
             Toetsen.Add("left");
             Toetsen.Add("right");
             Toetsen.Add("up");
-            Button btn = new Button(Toetsen[r.Next(0, 3)], r.Next(1, 4));
+            Button btn = new Button(Toetsen[r.Next(0, 3)], r.Next(1, 4), Convert.ToBoolean(r.Next(0, 1)));
             foreach (Button item in combos)
             {
                 combostemp.Add(item);
@@ -25,11 +26,13 @@ namespace Bruteforce_AI
             combostemp.Add(btn);
             return combostemp;
         }
-        public void UpdateScore(float score)
+        public void UpdateScore(float score, float time)
         {
             if (score > prevscore)
             {
                 prevscore = score;
+                prevtime = time;
+                combos.Clear();
                 foreach (Button item in combostemp)
                 {
                     combos.Add(item);
@@ -42,12 +45,14 @@ namespace Bruteforce_AI
     {
         public string cmd;
         public int presstime;
+        public bool jump;
 
         //Constructor
-        public Button(string cmd, int presstime)
+        public Button(string cmd, int presstime, bool jump)
         {
             this.cmd = cmd;
             this.presstime = presstime;
+            this.jump = jump;
         }
 
     }
