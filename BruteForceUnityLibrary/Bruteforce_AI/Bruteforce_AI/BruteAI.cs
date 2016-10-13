53f50f4e2590f3fb496534110ce2c7a2a862ce9e
Remi van Deursen <cryslo1994@gmail.com>
2016-10-13 12:58:21 +0200

    Cleanup

--- BruteForceUnityLibrary/Bruteforce_AI/Bruteforce_AI/BruteAI.cs
+++ BruteForceUnityLibrary/Bruteforce_AI/Bruteforce_AI/BruteAI.cs
@@ -12,8 +12,11 @@ namespace Bruteforce_AI
         float prevscore = 0;
         float score = 0;
         float prevtime;
+        float backupscore = 0;
         int timer = 0;
         int failtimer = 0;
+        int keycount = 0;
+        int jump = 0;
         bool presschange = false;
         bool receivescore = false;
         public List<Button> GenerateMovement()
@@ -22,18 +25,24 @@ namespace Bruteforce_AI
             if (score < prevscore)
             {
                 failtimer++;
-                if(failtimer == 4)
+                if(failtimer == 5)
                 {
                     failtimer = 0;
-                    combos.Remove(combos[combos.Count-1]);
+                    if (combos.Count == 1)
+                    {
+                        prevscore = 0;
+                        combos[combos.Count - 1].presstime = GetRandomNumber(0, 3);
+                    }
+                    else
+                    {
+                        prevscore = backupscore;
+                        combos.Remove(combos[combos.Count - 1]);
+                    }
                 }
-            }
-            else
-            {
-
             }
             if (score == prevscore && receivescore == true)
             {
+                failtimer = 0;
                 timer++;
                 if (timer >= 3)
                 {
@@ -41,6 +50,7 @@ namespace Bruteforce_AI
                     combos[r.Next(0, combos.Count)].presstime = r.Next(1, 4);
                     combos[r.Next(0, combos.Count)].jump = Convert.ToBoolean(r.Next(0, 2));
                     timer = 0;
+                    prevscore = backupscore;
                     presschange = true;
                     return combos;
                 }
@@ -65,7 +75,17 @@ namespace Bruteforce_AI
             List<string> Toetsen = new List<string>();
             Toetsen.Add("left");
             Toetsen.Add("right");
-            Button btn = new Button(Toetsen[r.Next(0, 2)], GetRandomNumber(0,3), Convert.ToBoolean(r.Next(0, 2)));
+            Button btn = new Button(Toetsen[keycount], GetRandomNumber(0,3), Convert.ToBoolean(jump));
+            keycount++;
+            if(keycount == 2) {
+                if (jump == 0) {
+                    jump = 1;
+                }
+                else {
+                    jump = 0;
+                }
+                keycount = 0;
+            }
             foreach (Button item in combos)
             {
                 combostemp.Add(item);
@@ -89,8 +109,12 @@ namespace Bruteforce_AI
             }
             else if (score > prevscore)
             {
+                backupscore = prevscore;
                 prevscore = score;
                 prevtime = time;
+                keycount = 0;
+                jump = 0;
+                failtimer = 0;
                 receivescore = true;
                 combos.Clear();
                 foreach (Button item in combostemp)
